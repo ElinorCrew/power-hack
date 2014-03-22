db03a7ec5b194897b078b4c96b11903ff0a4b681
Magnus Kirø <magnuskiro@gmail.com>
2014-03-22 01:39:54 +0100

    js cleanup

--- public/achievements/js/achievement-view-model.js
+++ public/achievements/js/achievement-view-model.js
@@ -8,22 +8,17 @@ var PH = this.PH || {};
     namespace.achievementViewModel = function(data) {
         var self = this;
 
+        //console.error(data.achievements[0].name); // = "Earth Hour"
         self.jsonData = data.achievements;
 
         // set correct image path for images.
         for(var i in self.jsonData) {
-
-            console.error(self.jsonData[i].explanation);
-
             // set image path
             self.jsonData[i].img = "style/images/" + self.jsonData[i].img + ".png";
 
             // set rel, enables overlay
             self.jsonData[i].rel = "" + self.jsonData[i].id;
-
         }
 
-        //console.error(self.jsonData[0].name); // = "Earth Hour"
-        //console.error(data.achievements[0].name); // = "Earth Hour"
     };
 }(PH));
