ae5fc41ce7f74626a114db7506ef0533f3aab4cb
teodoran <teodoran@stud.ntnu.no>
2014-03-22 07:51:37 +0100

    Real data in main view model.

--- public/main/js/main-view-model.js
+++ public/main/js/main-view-model.js
@@ -11,25 +11,25 @@ var PH = this.PH || {};
         self.jsonData = ko.observableArray(data.thisWeek);
 
         self.maxJsonData = ko.computed(function() {
-            return d3.max(self.jsonData(), function (d) {return d.val});
+            return _.max(self.jsonData(), function (d) { return d.val; });
         }, self);
 
         self.scaleBarData = function (value) {
             return value * 2 / self.maxJsonData();
-        }
+        };
 
-        self.averageGroupData = ko.computed(function() {
-            return 4
-        }, self);
+        self.averageGroupData = ko.observable(data.thisGroupAwg);
+
         self.myCurrentGroupData = ko.computed(function() {
-            return self.averageGroupData()-self.jsonData()[self.jsonData().length-1].val;
+            return self.averageGroupData() - self.jsonData()[self.jsonData().length - 1].val;
         }, self);
-        self.myAverageCurrentPowerData = ko.observable(2);
+
+        self.myAverageCurrentPowerData = ko.observable(data.lastWeekAwg);
 
         self.myCurrentPowerData = ko.computed(function() {
-            console.log(self.jsonData()[self.jsonData().length-2].val);
-            return self.myAverageCurrentPowerData()-self.jsonData()[self.jsonData().length-2].val;
+            // console.log(self.jsonData()[self.jsonData().length-2].val);
+            return self.myAverageCurrentPowerData() - self.jsonData()[self.jsonData().length - 2].val;
         }, self);
-    }
+    };
 }(PH));
 
